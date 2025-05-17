13db05469d42957b4f9ef18c04bf561ba6df782c
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2025-05-17 10:51:38 +0530

    int

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Rayzon3/go_project/internal/app"
+	"github.com/Rayzon3/go_project/internal/routes"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "Default server port")
+
+	app, err := app.NewApplication()
+
+	if err != nil {
+		panic(err)
+	}
+
+	r := routes.SetupRoutes(app)
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	app.Logger.Printf("Application Running on port %d \n", port)
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
